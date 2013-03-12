894d2ecdb12843b7788a4b604ab74776c7f28eda
matt wagers <mwagers@ucsc.edu>
2013-03-12 12:50:19 -0700

    getting started

--- datives.R
+++ datives.R
@@ -1,7 +1,12 @@
 bresnan <- read.csv(url("http://people.ucsc.edu/~mwagers/ling280/data/BresDative.txt"),
              header=TRUE, sep=" ")
 
+# for mosaic()
 install.packages("vcd")
 library(vcd)
+# for glmmPQL()
+install.packages("MASS")
+library(MASS)
+
 
 with(bresnan, table(prorec, proth, real)) -> pronominals
