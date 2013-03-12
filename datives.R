c9456c1143b172a5d685ca67ede4b424cf4a22c0
matt wagers <mwagers@ucsc.edu>
2013-03-12 14:51:05 -0700

    middle of class

--- datives.R
+++ datives.R
@@ -11,3 +11,8 @@ library(MASS)
 
 with(bresnan, table(proth, real)) -> pron.theme
 with(bresnan, table(prorec, real)) -> pron.recip
+
+mosaic(pron.theme, shade=TRUE)
+rbind(c(2104, 740), c(312,109)) ->expected
+
+mosaic(expected)
